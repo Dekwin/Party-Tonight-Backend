b5522ffea6cbecc237353c223b9ad560811b459c
thepiekie <thepiekie@icloud.com>
2017-05-17 08:55:37 +0300

    entities were updated for Order and Transaction classes

--- src/main/java/com/partymaker/mvc/model/business/order/OrderEntity.java
+++ src/main/java/com/partymaker/mvc/model/business/order/OrderEntity.java
@@ -11,84 +11,29 @@ public class OrderEntity implements Serializable {
 
     @Id
     @Column(name = "id_order")
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int id_order;
 
-    @Column(name = "customer")
-    private String customer;
+    @Column(name="subtotal")
+    private double subtotal;
 
-    @Column(name = "date_created")
-    private String date_created;
+    @Column(name = "seller_billing_email")
+    private String sellerBillingEmail;
+
+    @Column(name = "event_id")
+    private int eventId;
 
     @OneToMany(cascade = {CascadeType.ALL}, mappedBy = "order")
-    @Column(name = "bottles")
     private List<OrderedBottle> bottles = new ArrayList<>();
 
     @OneToOne(mappedBy = "order", cascade = {CascadeType.ALL})
-    @JoinColumn(name = "table")
     private OrderedTable table;
 
     @OneToOne(mappedBy = "order", cascade = {CascadeType.ALL})
-    @JoinColumn(name = "ticket")
     private OrderedTicket ticket;
 
     @ManyToOne
-    @JoinColumn(name = "id_transaction")
-    private Transaction id_transaction;
+    @JoinColumn(name = "transaction_id")
+    private Transaction transactionId;
 
-    public int getId_order() {
-        return id_order;
-    }
-
-    public void setId_order(int id_order) {
-        this.id_order = id_order;
-    }
-
-    public Transaction getId_transaction() {
-        return id_transaction;
-    }
-
-    public void setId_transaction(Transaction id_transaction) {
-        this.id_transaction = id_transaction;
-    }
-
-    public List<OrderedBottle> getBottles() {
-        return bottles;
-    }
-
-    public void setBottles(List<OrderedBottle> bottles) {
-        this.bottles = bottles;
-    }
-
-    public String getCustomer() {
-        return customer;
-    }
-
-    public void setCustomer(String customer) {
-        this.customer = customer;
-    }
-
-    public String getDate() {
-        return date_created;
-    }
-
-    public void setDate(String date) {
-        this.date_created = date;
-    }
-
-    public OrderedTable getTable() {
-        return table;
-    }
-
-    public void setTable(OrderedTable table) {
-        this.table = table;
-    }
-
-    public OrderedTicket getTicket() {
-        return ticket;
-    }
-
-    public void setTicket(OrderedTicket ticket) {
-        this.ticket = ticket;
-    }
 }
--- src/main/java/com/partymaker/mvc/model/business/order/Transaction.java
+++ src/main/java/com/partymaker/mvc/model/business/order/Transaction.java
@@ -14,111 +14,21 @@ public class Transaction implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "id_transaction")
-    private int id_transaction;
+    @Column(name = "id")
+    private int id;
 
-    @JsonProperty("billing_email")
-    @Column(name = "billing_email")
-    private String billingEmail;
+    @Column(name = "pay_key")
+    private String payKey;
 
-    @JsonProperty("subtotal")
-    @Column(name = "subtotal")
-    private double subtotal;
+    @Column(name = "service_tax")
+    private double serviceTax;
 
-    @JsonProperty("customer_email")
-    @Column(name = "customer_email")
-    private String customerEmail;
+    @Column(name = "customer_billing_email")
+    private String customerBillingEmail;
 
-    @JsonProperty("seller_email")
-    @Column(name = "seller_email")
-    private String sellerEmail;
+    @Column(name = "service_billing_email")
+    private String serviceBillingEmail;
 
-    @JsonProperty("id_event")
-    @Column(name = "id_event")
-    private int id_event;
-
-    @Column(name = "completed", columnDefinition = "0")
-    private int completed;
-
-    @OneToMany(mappedBy = "id_transaction", cascade = {CascadeType.ALL})
-    @Column(name = "order")
+    @OneToMany(mappedBy = "transaction", cascade = {CascadeType.ALL})
     private List<OrderEntity> order = new ArrayList<>();
-
-
-    public int getId_event() {
-        return id_event;
-    }
-
-    public void setId_event(int id_event) {
-        this.id_event = id_event;
-    }
-
-    @Override
-    public String toString() {
-        final StringBuilder sb = new StringBuilder("Transaction{");
-        sb.append("id_transaction=").append(id_transaction);
-        sb.append(", seller_email='").append(sellerEmail).append('\'');
-        sb.append(", customer_email='").append(customerEmail).append('\'');
-        sb.append(", billing_email='").append(billingEmail).append('\'');
-        sb.append(", subtotal='").append(subtotal).append('\'');
-        sb.append(", id_event=").append(id_event);
-        sb.append('}');
-        return sb.toString();
-    }
-
-    public int getId_transaction() {
-        return id_transaction;
-    }
-
-    public void setId_transaction(int id_transaction) {
-        this.id_transaction = id_transaction;
-    }
-
-    public String getBillingEmail() {
-        return billingEmail;
-    }
-
-    public void setBillingEmail(String billing_email) {
-        this.billingEmail = billing_email;
-    }
-
-    public double getSubtotal() {
-        return subtotal;
-    }
-
-    public void setSubtotal(double subtotal) {
-        this.subtotal = subtotal;
-    }
-
-    public String getCustomerEmail() {
-        return customerEmail;
-    }
-
-    public void setCustomerEmail(String customerEmail) {
-        this.customerEmail = customerEmail;
-    }
-
-    public String getSellerEmail() {
-        return sellerEmail;
-    }
-
-    public void setSellerEmail(String sellerEmail) {
-        this.sellerEmail = sellerEmail;
-    }
-
-    public int getCompleted() {
-        return completed;
-    }
-
-    public void setCompleted(int completed) {
-        this.completed = completed;
-    }
-
-    public List<OrderEntity> getOrder() {
-        return order;
-    }
-
-    public void setOrder(List<OrderEntity> order) {
-        this.order = order;
-    }
 }
